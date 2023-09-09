36b3f29017d031d300df74637f1f0a2fe98c9866
koy-hou <91444276+koy-hou@users.noreply.github.com>
2023-09-08 22:50:27 -0300

    Animaciones Ahora si que si terminado

--- proyectoPrograMovil/src/app/pages/menu-prin/menu-prin.page.ts
+++ proyectoPrograMovil/src/app/pages/menu-prin/menu-prin.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlertaService } from 'src/app/services/alerta.service';
+import { AnimationController } from '@ionic/angular';
+import type { Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu-prin',
@@ -9,18 +11,31 @@ import { AlertaService } from 'src/app/services/alerta.service';
 })
 export class MenuPrinPage implements OnInit {
 
-  nombre:string = ""
-  constructor(private router:Router, private alerta:AlertaService, private activatedRoute:ActivatedRoute) {  }
+  private animation!: Animation;
+
+  nombre: string = ""
+  constructor(private router: Router, 
+              private alerta: AlertaService, private activatedRoute: ActivatedRoute, private animationCtrl: AnimationController) { }
 
   async salir() {
-    var confirm = await this.alerta.confirmarAlert("¿Desea cerrar la sesión?","Confirmar","Cancelar");
-    if(confirm == true ) {
+    var confirm = await this.alerta.confirmarAlert("¿Desea cerrar la sesión?", "Confirmar", "Cancelar");
+    if (confirm == true) {
       this.router.navigateByUrl("login");
     }
   }
 
   ngOnInit() {
     this.nombre = this.activatedRoute.snapshot.params['estudiante'];
+  }
+
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(document.querySelectorAll("ion-button"))
+      .duration(1500)
+      .iterations(10)
+      .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
+      .fromTo('opacity', '1', '0.2');
   }
 
   visualizar() {
